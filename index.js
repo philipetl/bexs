48e482107d695da8e117358782a1bd8ccb0777d2
Philipe Terroso de Lima <cin_plima@uolinc.com>
2020-06-13 02:59:17 -0300

    incluindo recebimento de input do usuario via console

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const app = require('./src/config/custom-express');
+const readline = require('readline');
+const { exit } = require('process');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
+
+waitForUserInput = () => {
+    rl.question("please enter the route: ", function (queryRoute) {
+        if (queryRoute == "exit") {
+            rl.close();
+            exit();
+        } else {
+            console.log(queryRoute);
+            waitForUserInput();
+        }
+    });
+}
+
+waitForUserInput();
